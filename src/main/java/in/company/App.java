3b5a4f6dc7bbe406fbb89dfd8ce373c14452e4a2
dsparta <baboolmallick01@gmail.com>
2023-06-10 12:46:36 +0530

    maven app testing

---
+++ src/main/java/in/company/App.java
@@ -0,0 +1,13 @@
+package in.company;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
